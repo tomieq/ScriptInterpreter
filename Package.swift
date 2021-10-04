ece77d8029ecc2837edeba23bf0410bc3dbbcb06
tomieq <tomieq@wp.pl>
2021-10-04 12:23:53 +0200

    Write first test

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "IoTEngine",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "IoTEngine",
+            targets: ["IoTEngine"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -14,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "IoTEngine",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "IoTEngineTests",
             dependencies: ["IoTEngine"]),
---
+++ Sources/IoTEngine/Lexer.swift
@@ -0,0 +1,14 @@
+//
+//  Lexer.swift
+//  
+//
+//  Created by Tomasz Kucharski on 04/10/2021.
+//
+
+import Foundation
+
+class Lexer {
+    init(code: String) {
+        
+    }
+}
