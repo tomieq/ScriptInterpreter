82b48965a682c4b4cf6a95fb32b91fef38c8a546
tomieq <tomieq@wp.pl>
2022-01-28 11:53:39 +0100

    Implemented Value

---
+++ Sources/IoTEngine/Value.swift
@@ -0,0 +1,15 @@
+//
+//  Value.swift
+//  
+//
+//  Created by Tomasz Kucharski on 28/01/2022.
+//
+
+import Foundation
+
+enum Value {
+    case string(String)
+    case integer(Int)
+    case float(Float)
+    case bool(Bool)
+}
