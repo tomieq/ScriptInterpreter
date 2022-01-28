f7ab9765fafba902d23040214d175c792a0e8dd6
tomieq <tomieq@wp.pl>
2022-01-28 22:25:14 +0100

    Added ParserUtilsTests

---
+++ Tests/IoTEngineTests/ParserUtilsTests.swift
@@ -0,0 +1,44 @@
+//
+//  ParserUtilsTests.swift
+//  
+//
+//  Created by Tomasz Kucharski on 28/01/2022.
+//
+
+import Foundation
+import XCTest
+@testable import IoTEngine
+
+class ParserUtilsTests: XCTestCase {
+    
+    func test_convertIntTokenToValue() {
+        let valueRegistry = ValueRegistry()
+        let value = ParserUtils.token2Value(.intLiteral(45), valueRegistry: valueRegistry)
+        XCTAssertEqual(value, .integer(45))
+    }
+    
+    func test_convertBoolTokenToValue() {
+        let valueRegistry = ValueRegistry()
+        let value = ParserUtils.token2Value(.boolLiteral(true), valueRegistry: valueRegistry)
+        XCTAssertEqual(value, .bool(true))
+    }
+    
+    func test_convertFloatTokenToValue() {
+        let valueRegistry = ValueRegistry()
+        let value = ParserUtils.token2Value(.floatLiteral(9.12), valueRegistry: valueRegistry)
+        XCTAssertEqual(value, .float(9.12))
+    }
+    
+    func test_convertStringTokenToValue() {
+        let valueRegistry = ValueRegistry()
+        let value = ParserUtils.token2Value(.stringLiteral("hello world"), valueRegistry: valueRegistry)
+        XCTAssertEqual(value, .string("hello world"))
+    }
+    
+    func test_convertVariableTokenToValue() {
+        let valueRegistry = ValueRegistry()
+        valueRegistry.registerValue(name: "data", value: .integer(1985))
+        let value = ParserUtils.token2Value(.variable(name: "data"), valueRegistry: valueRegistry)
+        XCTAssertEqual(value, .integer(1985))
+    }
+}
