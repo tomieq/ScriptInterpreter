53f36245a3e87f358325bbbe38784ba3d869feeb
tomieq <tomieq@wp.pl>
2022-01-28 13:12:16 +0100

    Implemented calling function with arguments

--- Sources/IoTEngine/Parser.swift
+++ Sources/IoTEngine/Parser.swift
@@ -8,22 +8,41 @@
 import Foundation
 
 class Parser {
-    private let lexer: Lexer
+    private let lexicalAnalizer: LexicalAnalyzer
     private let functionRegistry: FunctionRegistry
     
-    init(lexer: Lexer, functionRegistry: FunctionRegistry) {
-        self.lexer = lexer
+    init(lexicalAnalizer: LexicalAnalyzer, functionRegistry: FunctionRegistry) {
+        self.lexicalAnalizer = lexicalAnalizer
         self.functionRegistry = functionRegistry
     }
     
     func execute() throws {
-        for token in self.lexer.tokens {
+        for (index, token) in self.lexicalAnalizer.lexer.tokens.enumerated() {
             switch token {
             case .function(let name):
                 try self.functionRegistry.callFunction(name: name)
+            case .functionWithArguments(let name):
+                let tokens = self.lexicalAnalizer.getTokensBetweenBrackets(indexOfOpeningBracket: index + 1)
+                try self.functionRegistry.callFunction(name: name, args: tokens.compactMap { self.token2Value($0) })
+                break
             default:
                 break
             }
+        }
+    }
+    
+    private func token2Value(_ token: Token) -> Value? {
+        switch token {
+        case .intLiteral(let value):
+            return .integer(value)
+        case .stringLiteral(let value):
+            return .string(value)
+        case .boolLiteral(let value):
+            return .bool(value)
+        case .floatLiteral(let value):
+            return .float(value)
+        default:
+            return nil
         }
     }
 }
--- Tests/IoTEngineTests/ParserTests.swift
+++ Tests/IoTEngineTests/ParserTests.swift
@@ -19,7 +19,8 @@ class ParserTests: XCTestCase {
         do {
             let script = "increaseCounter()"
             let lexer = try Lexer(code: script)
-            let parser = Parser(lexer: lexer, functionRegistry: functionRegistry)
+            let lexicalAnalizer = LexicalAnalyzer(lexer: lexer)
+            let parser = Parser(lexicalAnalizer: lexicalAnalizer, functionRegistry: functionRegistry)
             XCTAssertEqual(spy.callCounter, 0)
             XCTAssertNoThrow(try parser.execute())
             XCTAssertEqual(spy.callCounter, 1)
@@ -36,22 +37,40 @@ class ParserTests: XCTestCase {
         XCTAssertNoThrow(try functionRegistry.registerFunc(name: "increaseCounter", function: spy.increaseByOne))
         XCTAssertNoThrow(try functionRegistry.registerFunc(name: "addTwo", function: spy.increaseByTwo))
         
+
+        let script = "increaseCounter() addTwo()"
+        let lexer = try Lexer(code: script)
+        let lexicalAnalizer = LexicalAnalyzer(lexer: lexer)
+        let parser = Parser(lexicalAnalizer: lexicalAnalizer, functionRegistry: functionRegistry)
+        XCTAssertEqual(spy.callCounter, 0)
+        XCTAssertNoThrow(try parser.execute())
+        XCTAssertEqual(spy.callCounter, 3)
+
+    }
+    
+    func test_callFunctionWithBoolArgument() {
+        let spy = FunctionCallSpy()
+        let functionRegistry = FunctionRegistry()
+        XCTAssertNoThrow(try functionRegistry.registerFunc(name: "addValues", function: spy.addValues))
+        
+        let script = "addValues(true)"
         do {
-            let script = "increaseCounter() addTwo()"
             let lexer = try Lexer(code: script)
-            let parser = Parser(lexer: lexer, functionRegistry: functionRegistry)
-            XCTAssertEqual(spy.callCounter, 0)
+            let lexicalAnalizer = LexicalAnalyzer(lexer: lexer)
+            let parser = Parser(lexicalAnalizer: lexicalAnalizer, functionRegistry: functionRegistry)
+            XCTAssertEqual(spy.received.count, 0)
             XCTAssertNoThrow(try parser.execute())
-            XCTAssertEqual(spy.callCounter, 3)
-            
+            XCTAssertEqual(spy.received.count, 1)
+            XCTAssertTrue(spy.received.contains(Value.bool(true)))
         } catch {
-            
+            XCTAssert(false, error.localizedDescription)
         }
     }
 }
 
 fileprivate class FunctionCallSpy {
     var callCounter = 0
+    var received: [Value] = []
     
     func increaseByOne() {
         self.callCounter += 1
@@ -59,5 +78,9 @@ fileprivate class FunctionCallSpy {
     
     func increaseByTwo() {
         self.callCounter += 2
+    }
+    
+    func addValues(_ data: [Value]) {
+        self.received.append(contentsOf: data)
     }
 }
