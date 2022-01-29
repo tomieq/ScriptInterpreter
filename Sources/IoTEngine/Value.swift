94568bdedffefedcecf08e9028b8b3c1f2141160
tomieq <tomieq@wp.pl>
2022-01-29 16:44:24 +0100

    Make Value CustomStringConvertible

--- Sources/IoTEngine/Value.swift
+++ Sources/IoTEngine/Value.swift
@@ -28,3 +28,17 @@ public enum Value {
 }
 
 extension Value: Equatable {}
+extension Value: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .string(let value):
+            return value
+        case .integer(let value):
+            return "\(value)"
+        case .float(let value):
+            return "\(value)"
+        case .bool(let value):
+            return "\(value)"
+        }
+    }
+}
