e566f3c87a97ae2cd62bf5f1333cb9e5aa71c4f3
tomieq <tomieq@wp.pl>
2022-01-29 15:29:12 +0100

    Implemented namespaces in ValueRegistry

--- Sources/IoTEngine/ValueRegistry.swift
+++ Sources/IoTEngine/ValueRegistry.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+enum ValueRegistryError: Error {
+    case valueDoesNotExist(name: String)
+    case typeMismatch(variableName: String, existingType: String, newType: String)
+}
+
+extension ValueRegistryError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .valueDoesNotExist(let name):
+            return NSLocalizedString("ValueRegistryError.valueDoesNotExist: variable \(name) does not exist", comment: "ValueRegistryError")
+        case .typeMismatch(let variableName, let existingType, let newType):
+            return NSLocalizedString("ValueRegistryError.typeMismatch: variable \(variableName) is \(existingType) but \(newType) is trying to be set", comment: "ValueRegistryError")
+        }
+    }
+}
+
 struct ValueContainer {
     var value: Value?
     
@@ -16,21 +32,39 @@ struct ValueContainer {
 }
 
 class ValueRegistry {
+    private let upperValueRegistry: ValueRegistry?
     private var values: [String: ValueContainer] = [:]
+    
+    init(upperValueRegistry: ValueRegistry? = nil) {
+        self.upperValueRegistry = upperValueRegistry
+    }
     
     func registerValue(name: String, value: Value?) {
         self.values[name] = ValueContainer(value)
     }
     
-    func setValue(name: String, value: Value?) {
-        self.values[name] = ValueContainer(value)
+    func updateValue(name: String, value: Value?) throws {
+        if let oldValue = self.values[name] {
+            if let oldValueType = oldValue.value?.type, let newValueType = oldValue.value?.type {
+                guard oldValueType == newValueType else {
+                    throw ValueRegistryError.typeMismatch(variableName: name, existingType: oldValueType, newType: newValueType)
+                }
+            }
+            self.values[name] = ValueContainer(value)
+            return
+        }
+        if let upperValueRegistry = self.upperValueRegistry {
+            try upperValueRegistry.updateValue(name: name, value: value)
+            return
+        }
+        throw ValueRegistryError.valueDoesNotExist(name: name)
     }
     
     func getValue(name: String) -> Value? {
-        return self.values[name]?.value
+        return self.values[name]?.value ?? self.upperValueRegistry?.getValue(name: name)
     }
     
     func valueExists(name: String) -> Bool {
-        return self.values[name] != nil
+        return self.values[name] != nil || (self.upperValueRegistry?.valueExists(name: name) ?? false)
     }
 }
--- Tests/IoTEngineTests/ValueRegistryTests.swift
+++ Tests/IoTEngineTests/ValueRegistryTests.swift
@@ -25,4 +25,73 @@ final class ValueRegistryTests: XCTestCase {
         XCTAssertTrue(registry.valueExists(name: "amount"))
         XCTAssertEqual(registry.getValue(name: "amount"), .integer(20))
     }
+    
+    func test_reisterVariableInHigherNamespace() {
+        let outer = ValueRegistry()
+        let inner = ValueRegistry(upperValueRegistry: outer)
+        XCTAssertFalse(outer.valueExists(name: "amount"))
+        XCTAssertFalse(inner.valueExists(name: "amount"))
+        outer.registerValue(name: "amount", value: .integer(31))
+        XCTAssertTrue(outer.valueExists(name: "amount"))
+        XCTAssertTrue(inner.valueExists(name: "amount"))
+        
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(31))
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(31))
+    }
+    
+    func test_reisterVariableInLowerNamespace() {
+        let outer = ValueRegistry()
+        let inner = ValueRegistry(upperValueRegistry: outer)
+        XCTAssertFalse(outer.valueExists(name: "amount"))
+        XCTAssertFalse(inner.valueExists(name: "amount"))
+        inner.registerValue(name: "amount", value: .integer(31))
+        XCTAssertFalse(outer.valueExists(name: "amount"))
+        XCTAssertTrue(inner.valueExists(name: "amount"))
+        
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(31))
+        XCTAssertNil(outer.getValue(name: "amount"))
+    }
+    
+    func test_reisterVariableInBothNamespaces() {
+        let outer = ValueRegistry()
+        let inner = ValueRegistry(upperValueRegistry: outer)
+        XCTAssertFalse(outer.valueExists(name: "amount"))
+        XCTAssertFalse(inner.valueExists(name: "amount"))
+        
+        outer.registerValue(name: "amount", value: .integer(20))
+        inner.registerValue(name: "amount", value: .integer(100))
+        
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(100))
+    }
+    
+    func test_updateVariableInHigherNamespace() {
+        let outer = ValueRegistry()
+        let inner = ValueRegistry(upperValueRegistry: outer)
+        
+        outer.registerValue(name: "amount", value: .integer(20))
+        
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(20))
+        
+        XCTAssertNoThrow(try outer.updateValue(name: "amount", value: .integer(50)))
+        
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(50))
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(50))
+    }
+    
+    func test_updateVariableInLowerNamespace() {
+        let outer = ValueRegistry()
+        let inner = ValueRegistry(upperValueRegistry: outer)
+        
+        outer.registerValue(name: "amount", value: .integer(20))
+        
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(20))
+        
+        XCTAssertNoThrow(try inner.updateValue(name: "amount", value: .integer(50)))
+        
+        XCTAssertEqual(outer.getValue(name: "amount"), .integer(50))
+        XCTAssertEqual(inner.getValue(name: "amount"), .integer(50))
+    }
 }
