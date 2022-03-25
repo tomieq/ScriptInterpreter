ef3b9a7e5152409402dfcf7f92cf05a7f17497d9
tomieq <tomieq@wp.pl>
2022-03-25 17:04:24 +0100

    Adjusted VariableRegistryTests to new codebase

--- Tests/ScriptInterpreterTests/VariableRegistryTests.swift
+++ Tests/ScriptInterpreterTests/VariableRegistryTests.swift
@@ -9,30 +9,46 @@ import Foundation
 import XCTest
 @testable import ScriptInterpreter
 
+fileprivate extension VariableRegistry {
+    func registerVariable(name: String, value: Value) throws {
+        try self.registerVariable(name: name, variable: .primitive(value))
+    }
+    
+    func getValue(name: String) -> Value? {
+        guard let variable = self.getVariable(name: name) else { return nil }
+        switch variable {
+        case .primitive(let value):
+            return value
+        case .class(_, _):
+            return nil
+        }
+    }
+}
+
 final class VariableRegistryTests: XCTestCase {
     func test_registerNullVariable() {
         let registry = VariableRegistry()
-        XCTAssertFalse(registry.valueExists(name: "amount"))
-        XCTAssertNoThrow(try registry.registerValue(name: "amount", value: nil))
-        XCTAssertTrue(registry.valueExists(name: "amount"))
+        XCTAssertFalse(registry.variableExists(name: "amount"))
+        XCTAssertNoThrow(try registry.registerVariable(name: "amount", variable: nil))
+        XCTAssertTrue(registry.variableExists(name: "amount"))
     }
 
     func test_registerNonNullVariable() {
         let registry = VariableRegistry()
-        XCTAssertFalse(registry.valueExists(name: "amount"))
-        XCTAssertNoThrow(try registry.registerValue(name: "amount", value: .integer(20)))
-        XCTAssertTrue(registry.valueExists(name: "amount"))
+        XCTAssertFalse(registry.variableExists(name: "amount"))
+        XCTAssertNoThrow(try registry.registerVariable(name: "amount", value: .integer(20)))
+        XCTAssertTrue(registry.variableExists(name: "amount"))
         XCTAssertEqual(registry.getValue(name: "amount"), .integer(20))
     }
 
     func test_reisterVariableInHigherNamespace() {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
-        XCTAssertFalse(outer.valueExists(name: "amount"))
-        XCTAssertFalse(inner.valueExists(name: "amount"))
-        XCTAssertNoThrow(try outer.registerValue(name: "amount", value: .integer(31)))
-        XCTAssertTrue(outer.valueExists(name: "amount"))
-        XCTAssertTrue(inner.valueExists(name: "amount"))
+        XCTAssertFalse(outer.variableExists(name: "amount"))
+        XCTAssertFalse(inner.variableExists(name: "amount"))
+        XCTAssertNoThrow(try outer.registerVariable(name: "amount", value: .integer(31)))
+        XCTAssertTrue(outer.variableExists(name: "amount"))
+        XCTAssertTrue(inner.variableExists(name: "amount"))
 
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(31))
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(31))
@@ -41,11 +57,11 @@ final class VariableRegistryTests: XCTestCase {
     func test_reisterVariableInLowerNamespace() {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
-        XCTAssertFalse(outer.valueExists(name: "amount"))
-        XCTAssertFalse(inner.valueExists(name: "amount"))
-        XCTAssertNoThrow(try inner.registerValue(name: "amount", value: .integer(31)))
-        XCTAssertFalse(outer.valueExists(name: "amount"))
-        XCTAssertTrue(inner.valueExists(name: "amount"))
+        XCTAssertFalse(outer.variableExists(name: "amount"))
+        XCTAssertFalse(inner.variableExists(name: "amount"))
+        XCTAssertNoThrow(try inner.registerVariable(name: "amount", value: .integer(31)))
+        XCTAssertFalse(outer.variableExists(name: "amount"))
+        XCTAssertTrue(inner.variableExists(name: "amount"))
 
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(31))
         XCTAssertNil(outer.getValue(name: "amount"))
@@ -54,11 +70,11 @@ final class VariableRegistryTests: XCTestCase {
     func test_reisterVariableInBothNamespaces() {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
-        XCTAssertFalse(outer.valueExists(name: "amount"))
-        XCTAssertFalse(inner.valueExists(name: "amount"))
+        XCTAssertFalse(outer.variableExists(name: "amount"))
+        XCTAssertFalse(inner.variableExists(name: "amount"))
 
-        XCTAssertNoThrow(try outer.registerValue(name: "amount", value: .integer(20)))
-        XCTAssertNoThrow(try inner.registerValue(name: "amount", value: .integer(100)))
+        XCTAssertNoThrow(try outer.registerVariable(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try inner.registerVariable(name: "amount", value: .integer(100)))
 
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(100))
@@ -68,12 +84,12 @@ final class VariableRegistryTests: XCTestCase {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
 
-        XCTAssertNoThrow(try outer.registerValue(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try outer.registerVariable(name: "amount", value: .integer(20)))
 
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(20))
 
-        XCTAssertNoThrow(try outer.updateValue(name: "amount", value: .integer(50)))
+        XCTAssertNoThrow(try outer.updateVariable(name: "amount", variable: .primitive(.integer(50))))
 
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(50))
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(50))
@@ -83,12 +99,12 @@ final class VariableRegistryTests: XCTestCase {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
 
-        XCTAssertNoThrow(try outer.registerValue(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try outer.registerVariable(name: "amount", value: .integer(20)))
 
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(20))
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(20))
 
-        XCTAssertNoThrow(try inner.updateValue(name: "amount", value: .integer(50)))
+        XCTAssertNoThrow(try inner.updateVariable(name: "amount", variable: .primitive(.integer(50))))
 
         XCTAssertEqual(outer.getValue(name: "amount"), .integer(50))
         XCTAssertEqual(inner.getValue(name: "amount"), .integer(50))
@@ -97,16 +113,16 @@ final class VariableRegistryTests: XCTestCase {
     func test_updateConstant() {
         let registry = VariableRegistry()
 
-        XCTAssertNoThrow(try registry.registerConstant(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try registry.registerConstant(name: "amount", variable: .primitive(.integer(20))))
         XCTAssertEqual(registry.getValue(name: "amount"), .integer(20))
-        XCTAssertThrowsError(try registry.updateValue(name: "amount", value: .integer(50)))
+        XCTAssertThrowsError(try registry.updateVariable(name: "amount", variable: .primitive(.integer(50))))
     }
 
     func test_memoryDump() {
         let outer = VariableRegistry()
         let inner = VariableRegistry(topVariableRegistry: outer)
 
-        XCTAssertNoThrow(try outer.registerValue(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try outer.registerVariable(name: "amount", value: .integer(20)))
 
         XCTAssertEqual(outer.memoryDump()["amount"], .integer(20))
         XCTAssertEqual(inner.memoryDump()["amount"], .integer(20))
@@ -115,7 +131,7 @@ final class VariableRegistryTests: XCTestCase {
     func test_clearMemory() {
         let registry = VariableRegistry()
 
-        XCTAssertNoThrow(try registry.registerValue(name: "amount", value: .integer(20)))
+        XCTAssertNoThrow(try registry.registerVariable(name: "amount", value: .integer(20)))
         XCTAssertEqual(registry.getValue(name: "amount"), .integer(20))
         registry.clearMemory()
         XCTAssertNil(registry.getValue(name: "amount"))
